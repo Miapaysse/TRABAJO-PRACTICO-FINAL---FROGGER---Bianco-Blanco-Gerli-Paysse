78e3133ff9c032a5f213857d5a19ab92aac432f8
Ankx7 <agerl@itba.edu.ar>
2026-07-12 01:19:30 -0400

    Borre funciones que no van.

--- FrontRaspi/frontend.h
+++ FrontRaspi/frontend.h
@@ -25,21 +25,6 @@
  ******************************************************************************/
 
 /**
- * @brief Dibuja la rana segun las coordenadas recibidas del backend
-*/
-void moveFrog(int x, int y);
-
-/**
- * @brief Dibuja la obstaculos segun las coordenadas recibidas del backend y que tipo de obstaculo es auto/tronco
-*/
-void drawObstacle(int x, int y, int length, uint8_t type);
-
-/**
- * @brief Imprime puntaje en display segun las calculos del backend
-*/
-void printScore(int score, int mode);
-
-/**
  * @brief Funciones para incializar, actualizar y apagar frontend segun backend
 */
 void frontendInit(void);
