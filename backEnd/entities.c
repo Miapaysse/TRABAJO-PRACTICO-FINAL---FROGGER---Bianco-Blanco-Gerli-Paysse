2384b9690bcec70e10ef3f90282e70cb202972f9
Miapaysse <162704754+Miapaysse@users.noreply.github.com>
2026-07-14 19:39:58 -0300

    Agrego funcion create entities

--- backEnd/entities.c
+++ backEnd/entities.c
@@ -87,10 +87,65 @@ void frogDies(int* lives, GameStateId * id ){
   }
 }
 
-void createEntities(){}
+void createEntities(Game* p2game, GameEntities* p2GameEntities, Level* p2level){
+    //inicializamos para que no haya entidades activas en un principio
+    int i, j, y;
+    for (i=0; i<MAX_OBSTACLES; i++){
+        (p2GameEntities -> obstacles[i]).active = false;
+    }
+    for (j=0; j<MAX_FLOATERS; j++){
+        (p2GameEntities -> floaters[j]).active = false;
+    }
+
+    //recorremos filas y vamos creando obstáculos en cada una
+    int num_level; //FALTA SABER DE DONDE SACO EL NRO DE NIVEL
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+
+        Zone zone_type = p2level -> zones[y]; //guardamos qué tipo de zona es esta fila particular
+
+        if (zone_type == ROAD) {
+          int obs_base = 2 + (num_level / 2); 
+          int obs_variation = rand() % 2; //da 1 o 0
+          int num_obs = obs_base + obs_variation;
+
+          int space_btween = MAP_WIDTH / num_obs;
+          int obs_length = 1 + (rand() % 2);
+
+          //recorro arreglo de obstaculos
+          int t;
+          for (t=0; t<num_obs; t++){
+            (p2GameEntities->obstacles[t]).active = true;
+            (p2GameEntities->obstacles[t]).y = y;
+            (p2GameEntities->obstacles[t]).x = t + space_btween + 1;
+            (p2GameEntities->obstacles[t]).y = y;
+            (p2GameEntities->obstacles[t]).height = 1;
+            (p2GameEntities->obstacles[t]).length = obs_length;
+            (p2GameEntities->obstacles[t]).speed= 1 + (num_level/2);
+
+            //si la fila es nro. par los obstaculos van en un sentido, sino en el contrario
+            if (y/2){
+                (p2GameEntities->obstacles[t]).direction = DIR_LEFT;
+            }
+            else {
+                (p2GameEntities->obstacles[t]).direction = DIR_RIGHT;
+            }
+
+            (p2GameEntities->obstacles[t]).type = OBSTACLE;
+          }
+
+        } 
+        else if (zone_type == WATER) {
+            
+        }
+    }
+}
 
 void updateEntities(){
-
+    for(i=0; i<MAX_OBSTACLES; i++){
+        if(collided(frog,&(obstacles[i])) ){
+            return 1;
+        }
+    }
 }
 
 /*******************************************************************************
