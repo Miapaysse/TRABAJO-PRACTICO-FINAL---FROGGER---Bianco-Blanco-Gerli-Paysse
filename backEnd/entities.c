7eba30b8cdccc0de70fbab0975cdee5f8d9b2b7c
Miapaysse <162704754+Miapaysse@users.noreply.github.com>
2026-07-14 20:50:27 -0300

    Cambios varios, incluido correcion de typo en game.h

--- backEnd/entities.c
+++ backEnd/entities.c
@@ -87,24 +87,25 @@ void frogDies(int* lives, GameStateId * id ){
   }
 }
 
-void createEntities(Game* p2game, GameEntities* p2GameEntities, Level* p2level){
+void createEntities(GameEntities* gameEntities, Level* level){
     //inicializamos para que no haya entidades activas en un principio
     int i, j, y;
     for (i=0; i<MAX_OBSTACLES; i++){
-        (p2GameEntities -> obstacles[i]).active = false;
+        (gameEntities -> obstacles[i]).active = false;
     }
     for (j=0; j<MAX_FLOATERS; j++){
-        (p2GameEntities -> floaters[j]).active = false;
+        (gameEntities -> floaters[j]).active = false;
     }
 
     //recorremos filas y vamos creando obstáculos en cada una
-    int num_level; //FALTA SABER DE DONDE SACO EL NRO DE NIVEL
+    
     for (int y = 0; y < MAP_HEIGHT; y++) {
 
-        Zone zone_type = p2level -> zones[y]; //guardamos qué tipo de zona es esta fila particular
+        ZoneType zone_type = level -> zones[y]; //guardamos qué tipo de zona es esta fila particular
+
 
         if (zone_type == ROAD) {
-          int obs_base = 2 + (num_level / 2); 
+          int obs_base = 2 + (level->id / 2); 
           int obs_variation = rand() % 2; //da 1 o 0
           int num_obs = obs_base + obs_variation;
 
@@ -114,23 +115,23 @@ void createEntities(Game* p2game, GameEntities* p2GameEntities, Level* p2level){
           //recorro arreglo de obstaculos
           int t;
           for (t=0; t<num_obs; t++){
-            (p2GameEntities->obstacles[t]).active = true;
-            (p2GameEntities->obstacles[t]).y = y;
-            (p2GameEntities->obstacles[t]).x = t + space_btween + 1;
-            (p2GameEntities->obstacles[t]).y = y;
-            (p2GameEntities->obstacles[t]).height = 1;
-            (p2GameEntities->obstacles[t]).length = obs_length;
-            (p2GameEntities->obstacles[t]).speed= 1 + (num_level/2);
+            (gameEntities->obstacles[t]).active = true;
+            (gameEntities->obstacles[t]).y = y;
+            (gameEntities->obstacles[t]).x = t + space_btween + 1;
+            (gameEntities->obstacles[t]).y = y;
+            (gameEntities->obstacles[t]).height = 1;
+            (gameEntities->obstacles[t]).length = obs_length;
+            (gameEntities->obstacles[t]).speed= 1 + (level->id/2);
 
             //si la fila es nro. par los obstaculos van en un sentido, sino en el contrario
             if (y/2){
-                (p2GameEntities->obstacles[t]).direction = DIR_LEFT;
+                (gameEntities->obstacles[t]).direction = DIR_LEFT;
             }
             else {
-                (p2GameEntities->obstacles[t]).direction = DIR_RIGHT;
+                (gameEntities->obstacles[t]).direction = DIR_RIGHT;
             }
 
-            (p2GameEntities->obstacles[t]).type = OBSTACLE;
+            (gameEntities->obstacles[t]).type = OBSTACLE;
           }
 
         } 
