c1a441ba066cf5bc594cf8aaaef97be43dec8026
anapblanco <anapblanco@itba.edu.ar>
2026-07-21 11:32:23 -0300

    funciones allegrofrontrnd.h comentadas

--- frontend/allegro/allegroFrontend.h
+++ frontend/allegro/allegroFrontend.h
@@ -27,10 +27,46 @@
   * @brief Funciones para incializar, actualizar y apagar frontend 
  */
 int frontendInit(void);
+
+/**
+ * @brief TODO: Esta funcion inicializa el frontend de Allegro. 
+ * @param none 
+ * @return devuelve un entero para control de errores
+*/
+
 Input_t frontendGetInput(void);
+
+/**
+ * @brief TODO: Esta funcion recibe input del usuario e informa al backend que tecla fue presionada.
+ * @param none
+ * @return Devuelve el tipo de input que fue recibido.
+*/
+
 void frontendRender(Game_t * game);
+
+/**
+ * @brief TODO: Esta funcion actualiza el puntaje cada vez que la rana llega a una zona segura. 
+ * @param frog Recibe el puntero a la rana para analizar si llego a una zona segura
+ * @param score Recibe el puntero al puntaje 
+ * @return devuelve un entero para control de errores
+*/
+
 void frontendDestroy(void);
+
+/**
+ * @brief TODO: Esta funcion libera memoria y cierra recursos del frontend al finalizar. 
+ * @param none
+ * @return No devuelve nada.
+*/
+
 void frontManageError(ErrorCode_t err);
+
+/**
+ * @brief TODO: Esta funcion maneja los errores del frontend, se encarga de mostrarlos por
+ * pantalla o en consola en caso de no poder cargar los recursos suficientes. 
+ * @param err Recibe el código de error
+ * @return No devuelve nada.
+*/
 
 #endif
  
